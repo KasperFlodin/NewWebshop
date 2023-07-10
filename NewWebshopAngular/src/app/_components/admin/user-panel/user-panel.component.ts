d1380d794ddb36373429c645931aabb4291e0d70
Kasper Flodin <kasperflodin@gmail.com>
2023-07-10 08:58:23 +0200

    user panel

--- NewWebshopAngular/src/app/_components/admin/user-panel/user-panel.component.ts
+++ NewWebshopAngular/src/app/_components/admin/user-panel/user-panel.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User, resetUser } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   standalone: true,
   selector: 'app-user-panel',
   templateUrl: './user-panel.component.html',
+  imports: [CommonModule, FormsModule],
   styles: [
   ]
 })
-export class UserPanelComponent {
+export class UserPanelComponent implements OnInit {
+  message: string ='';
+  users: User[] = [];
+  user: User = resetUser();
+
+  constructor(
+    private userService: UserService,
+  ) { }
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe(u => this.users = u);
+  }
+
+  edit(user: User): void {
+    this.user = {
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      city: user.city,
+      address: user.address,
+      zip: user.zip,
+      phone: user.phone,
+      email: user.email,
+    }
+  }
+
+  delete(user: User): void {
+    if (confirm('Er du sikker på du vil slette?')) {
+      this.userService.delete(user.id).subscribe(() => {
+        this.users = this.users.filter(x => x.id != user.id)
+      });
+    }
+  }
+
+  cancel(): void {
+    this.message = '';
+    this.user = resetUser();
+  }
+
+  save(): void {
+    this.message = '';
+
+    if (this.user.id==0) {
+      this.userService.create(this.user).subscribe({
+        next: (x) => {
+          this.users.push(x);
+          this.user = resetUser();
+        },
+        error: (err) => {
+          this.message = Object.values(err.error.errors).join(', ');
+        }
+      });
+    }
+    else {
+      this.userService.update(this.user).subscribe({
+        error: (err) => {
+          this.message = Object.values(err.error.errors).join(', ');
+        },
+        complete: () => {
+
+
+
+          this.userService.getAll().subscribe(x => this.users = x);
+          this.user = resetUser();
+        }
+      });
+    }
+  }
+
+
 
 }
---
+++ NewWebshopAngular/src/app/_services/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiURL = environment.apiUrl + 'user';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiURL);
+  }
+
+  getById(userId: string): Observable<User> {
+    return this.http.get<User>(this.apiURL + '/'+ 'user' + '/' + userId);
+  }
+
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.apiURL, user);
+  }
+
+  update(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiURL}/${user.id}`, user);
+  }
+
+  delete(userId: number | undefined): Observable<User> {
+    return this.http.delete<User>(`${this.apiURL}/${userId}`);
+  }
+}
